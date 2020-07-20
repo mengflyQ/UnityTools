3a8bf17bc7d0e1048cec63f0eff0ba9e5c68927d
xinzhuzi <1487842110@qq.cin>
2020-07-20 12:43:32 +0800

    UGUI

---
+++ UGUI/UGUI/Assets/UGUI/Runtime/UI/Core/Empty4Raycast.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.UI
+{
+    /// <summary>
+    /// https://blog.uwa4d.com/archives/fillrate.html
+    /// 只在逻辑上响应Raycast,但是不参与绘制
+    /// </summary>
+    public class Empty4Raycast : MaskableGraphic
+    {
+        protected Empty4Raycast()
+        {
+            useLegacyMeshGeneration = false;
+        }
+
+        protected override void OnPopulateMesh(VertexHelper toFill)
+        {
+            toFill.Clear();
+        }
+    }
+}
