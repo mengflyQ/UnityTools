d35078cf695f16dce9afcfbbfed03a4552f451b2
xinzhuzi <1487842110@qq.cin>
2020-07-23 16:19:37 +0800

    AB

---
+++ AB/Assets/Editor/AssetBundleGraphTool/System/PrefabBuilders/PrefabCreateDescription.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Model=UnityEngine.AssetGraph.DataModel.Version2;
+using Object = UnityEngine.Object;
+
+namespace UnityEngine.AssetGraph {
+
+	public class PrefabCreateDescription
+	{
+		/// <summary>
+		/// Asset path to creating prefab.
+		/// </summary>
+		public string prefabName;
+		
+		/// <summary>
+		/// Paths to additional assets to take into account other than given objects from node, such as objects assigned via inspector.
+		/// </summary>
+		public List<string> additionalAssetPaths;
+
+		public PrefabCreateDescription()
+		{
+			additionalAssetPaths = new List<string>(32);
+		}
+
+		public void Reset()
+		{
+			prefabName = string.Empty;
+			additionalAssetPaths.Clear();
+		}
+	}
+}
---
+++ AB/Assets/Editor/AssetBundleGraphTool/System/Utility/LogUtility.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+
+namespace UnityEngine.AssetGraph {
+
+	public class LogUtility {
+
+		public static readonly string kTag = "AssetGraph";
+
+		private static Logger s_logger;
+
+		public static Logger Logger {
+			get {
+				if(s_logger == null) {
+					s_logger = new Logger(Debug.unityLogger.logHandler);
+                    ShowVerboseLog (UserPreference.DefaultVerboseLog);
+				}
+
+				return s_logger;
+			}
+		}
+
+        public static void ShowVerboseLog(bool bVerbose) {
+            var curValue = (bVerbose)? LogType.Log : LogType.Warning;
+            if (curValue != Logger.filterLogType) {
+                Logger.filterLogType = curValue;
+            }
+        }
+	}
+}
