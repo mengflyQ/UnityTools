9fb8b87df33f236d101d173eee8ea60547873be0
xinzhuzi <1487842110@qq.cin>
2020-08-10 21:29:22 +0800

    AM

--- AM/Assets/AM/AssetType.cs
+++ AM/Assets/AM/AssetType.cs
@@ -23,5 +23,6 @@ namespace AM
         PhysicMaterial,
         Script,
         DLL,
+        Lua,
     }
 }
---
+++ AM/Assets/AM/HotUpdateManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameBasicsLibrary;
+
+namespace AM
+{
+    /// <summary>
+    /// 热更管理
+    /// 支持整个配置表更新,将所有的热更包更新下载下来
+    /// 支持单个热更包更新
+    /// 最好的做法是对比完配置表,本地缺什么就下载什么.
+    /// 1. 下载服务器配置表
+    /// 2. 与本地配置表对比
+    /// 3. 进行单包下载,不建议下载一整个 zip 文件,有可能中断
+    /// 4. 下载完毕,进行检查
+    /// 5. 结束热更
+    /// </summary>
+    public class HotUpdateManager : Singleton<HotUpdateManager> , IManager
+    {
+        //配置表
+        public AssetsConfigs assetsConfigs;
+        public void Awake()
+        {
+            
+        }
+
+        public void Start()
+        {
+        
+        }
+
+        public void Update()
+        {
+        
+        }
+
+        public void LateUpdate()
+        {
+        
+        }
+        public void OnDestroy()
+        {
+        
+        }
+    }
+}
