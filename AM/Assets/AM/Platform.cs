814337d78776c406225ea7fd831724dd5aa0b729
xinzhuzi <1487842110@qq.cin>
2020-08-11 22:43:17 +0800

    AM

--- AM/Assets/AM/Platform.cs
+++ AM/Assets/AM/Platform.cs
@@ -52,36 +52,9 @@ namespace AM
                     return "unknow";
             } 
         }
-
-        /// <summary>
-        /// Application.streamingAssetsPath 是只读路径,无法在其他平台上面写入数据
-        /// Application.persistentDataPath  是可以读写路径
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public static string GetLocalStreamingAssetsPlatformPath(string path)
-        {
-            string lspPath = "";
-#if UNITY_STANDALONE || UNITY_EDITOR
-            lspPath = Application.streamingAssetsPath + "/" + GetPlatformName();
-#elif UNITY_IPHONE
-            lspPath = "file://" + Application.streamingAssetsPath + "/" + GetPlatformName();
-#elif UNITY_ANDROID
-            lspPath = "jar:file://" + Application.streamingAssetsPath + "/" + GetPlatformName();
-#else
-            lspPath = Application.streamingAssetsPath + "/" + GetPlatformName();
-#endif
-            if (!Directory.Exists(lspPath))
-            {
-                Directory.CreateDirectory(lspPath);
-            }
-
-            if (!Directory.Exists(lspPath))
-            {
-                Debug.LogError("不存在这个文件夹");
-            }
-            return lspPath + "/" + path;
-        }
+        
+        
+       
         
     }
 }
--- AM/Temp/gradleOut/launcher/build/intermediates/merged_assets/release/out/android/ReadMe.txt
+++
@@ -1,6 +0,0 @@
-# 简单介绍
-
-* 这是一个解决资源热更的方案.
-* 包含,随包资源,热更资源,下载,上传,对比等
-* 涉及到打包工具:
-https://gitee.com/chenxuejian/TJPackTool/
